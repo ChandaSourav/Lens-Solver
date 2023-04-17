74e3bda1d1bf04ddfcf1353b17d700910e618135
souravchanda <www.souravchanda445@gmail.com>
2023-04-17 14:12:06 +0530

    some changes

--- Website/app.js
+++ Website/app.js
@@ -12,3 +12,16 @@ takeSnapShot = function () {
           '<img src="' + data_uri + '" width="220px" height="190px" />';
   });
 }
+
+let uploadButton = document.getElementById("upload-button");
+let chosenImage = document.getElementById("chosen-image");
+let fileName = document.getElementById("file-name");
+
+uploadButton.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadButton.files[0]);
+    reader.onload = () => {
+        chosenImage.setAttribute("src",reader.result);
+    }
+    fileName.textContent = uploadButton.files[0].name;
+}
