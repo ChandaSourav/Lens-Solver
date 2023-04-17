23846678d72f3b7c36d7a2cbd9b765dbb79817ba
souravchanda <www.souravchanda445@gmail.com>
2023-04-17 13:14:55 +0530

    minor changes

--- Website/app.js
+++ Website/app.js
@@ -1,7 +1,7 @@
 Webcam.set({
   width: 220,
   height: 190,
-  image_format: 'jpeg',
+  image_format: 'png',
   jpeg_quality: 100
 });
 Webcam.attach('#camera');
@@ -9,6 +9,6 @@ Webcam.attach('#camera');
 takeSnapShot = function () {
   Webcam.snap(function (data_uri) {
       document.getElementById('snapShot').innerHTML = 
-          '<img src="' + data_uri + '" width="200px" height="200px" />';
+          '<img src="' + data_uri + '" width="220px" height="190px" />';
   });
 }
